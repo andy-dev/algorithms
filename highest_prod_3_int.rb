a30f50bf72169883ba59d309576315dc30b003c9
andy-dev <andres.macedo@gmail.com>
2015-04-21 13:15:12 -0700

    solve algo highest prod of 3 integers, still need to find better solution

---
+++ highest_prod_3_int.rb
@@ -0,0 +1,48 @@
+#Find the highest possible product of 3 integers in an array
+
+###### GIVEN ######
+
+  # array_of_integers
+  # array_of_integers.length >= 3
+
+###### Input | Output ######
+
+  # input: array of integers
+  # output: the highest product of 3 int
+
+###### Key Points ######
+
+  # What hapens if we have zero
+  # What happens if we hve negative numbers
+
+###### CODE #######
+
+  # Pseudo
+  # Sort the array
+  # multiply the last 3 integers--save result
+  # multiply the first 2 and the last integer -- save result
+  # return highest resutl
+
+  # lets work with a couple of possible arrays
+
+  array_one = [-1,0,3,5,1] #=> 15
+  array_two = [-10,-10,1,3,5] #=> 500
+  array_three = [-4,0,5,1] #=>0
+
+  def get_highest_product_of_three_int(array_of_integers)
+    results = []
+    array_of_integers.sort
+    results << array_of_integers[0] * array_of_integers[1] * array_of_integers[-1]
+    array_of_integers.reverse!
+    results << array_of_integers[0] * array_of_integers[1] * array_of_integers[2]
+    return results.max
+  end
+
+p get_highest_product_of_three_int(array_one) == 15
+p get_highest_product_of_three_int(array_two) == 500
+p get_highest_product_of_three_int(array_three) == 0
+
+# notes
+# will have an O(nlgn) time O(n) space #(note to self check for correctness)
+
+
---
+++ integer_product_array_algorithm.rb
@@ -0,0 +1,28 @@
+# Array of integers and for each index you want product of all integers exept integers at that index
+
+###### GIVEN ######
+
+array_of_integers = [ 1, 7, 3, 4]
+
+###### Input | Output ######
+
+  # input: array of integers
+  # output: product_of_integers = [84, 12, 28, 21]
+
+###### Key Points ######
+
+  # We do not want the product of the current index
+
+###### CODE #######
+
+  def get_product_integers(array_of_integers)
+
+
+    # get all products after the index
+    # get all products before the index
+
+
+
+  end
+
+get_product_integers(array_of_integers)
