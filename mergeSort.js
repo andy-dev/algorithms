fe29690f5f2d58a32a399f17cb7c8fd7d78b4b5f
andy-dev <andres.macedo@gmail.com>
2015-05-26 00:04:48 -0700

    add some minor fixes to mergeSort JS

--- mergeSort.js
+++ mergeSort.js
@@ -1,9 +1,9 @@
-var mergeSort = function(array){
+var mergeSort = function(myArray){
 
   var merge = function(leftSide, rightSide){
-    var results = [];
-    var l = 0;
-    var r = 0;
+    var results = []
+    var l = 0
+    var r = 0
 
     while (!( r >= rightSide.length && l >= leftSide.length )) {
       if (r >= rightSide.length || ((l < leftSide.length) && (leftSide[l] < rightSide[r])) ){
@@ -15,9 +15,13 @@ var mergeSort = function(array){
         r++
       }
     };
-    console.log(results)
+
+    if(results.length === myArray.length){
+      console.log(results)
+    };
     return results
-  }
+  };
+
 
   var spliter = function(arr){
     if(arr.length <= 1){
@@ -25,16 +29,17 @@ var mergeSort = function(array){
     };
 
     var mid = (arr.length/2)
-    var leftSorted = spliter(arr.slice(0, mid));
+    var leftSorted = spliter(arr.slice(0, mid))
 
-    var rightSorted = spliter(arr.slice(mid, arr.length));
+    var rightSorted = spliter(arr.slice(mid, arr.length))
     return merge(leftSorted,rightSorted);
   };
 
-  spliter(array);
+  spliter(myArray)
 }
 
 mergeSort([4,3,2,1])
+mergeSort([6,7,5,3,5,6,7,8,9,0,0,6,5,])
 
 
 
