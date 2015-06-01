f8a6d6f49f9773311d4cbf1faf30331eec0fed30
andy-dev <andres.macedo@gmail.com>
2015-06-01 10:23:50 -0700

    fix minor var in marksort js

--- markSort.js
+++ markSort.js
@@ -4,18 +4,18 @@ var arrMin = Math.min.apply(Math, arr)
 var diff = arrMax - arrMin + 1
 var markings = []
 
-for(i = 0; i < diff; i++){
+for(var i = 0; i < diff; i++){
   markings.push(0);
 }
 
-for(i = 0; i < arr.length; i++){
+for(var i = 0; i < arr.length; i++){
   markings[arr[i] - arrMin] = 1;
 }
 
 var result = [];
 
 var sortedResult = (function (){
-  for(i = 0; i <= markings.length; i++){
+  for(var i = 0; i <= markings.length; i++){
     if(markings[i] === 1){
       result.push(i + arrMin)
     }
