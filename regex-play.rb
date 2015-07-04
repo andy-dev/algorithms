f1b255521674ff6af4564696304c0736b612dbbc
andy-dev <andres.macedo@gmail.com>
2015-07-04 14:53:07 -0700

    add example regex method and gsub

--- regex-play.rb
+++ regex-play.rb
@@ -13,5 +13,22 @@ p /\d\d:\d\d (AM|PM)/ =~ "where is our 10:08 PM date in the string?"
 #=> 13, we matched at 13
 
 night_time = "11:11 PM"
+wakey_wakey = "11:11 am"
 p "Its time to go sleepy sleepy" if /PM/ =~ night_time
 
+# regex is case sensitve so if we want to match am and AM
+p "Wakey Wakey eggs and Bakey" if /AM/i =~ wakey_wakey
+
+# using gsub and regex in a method
+
+def changer_sub(content)
+
+  if content =~ /Superman/
+    puts "did you say Superman!!"
+    p "no sir, I think you said:"
+  end
+
+  p content.gsub( /Superman/, "Batman")
+end
+
+changer_sub("Superman is my favorite superhero")
